a85158297db15e15f80d13b674908f50e321fa29
Evan Martin <evan.martin@shaw.ca>
2017-08-04 12:41:35 -0700

    updated README.md, added input and output folders for scripts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # py-scripts
+
+Some python scripts
+
+### tint.py
+
+tints an image
+
+### rainbowify.py
+
+saves different versions of an image, each under a different tint
--- rainbowify.py
+++ rainbowify.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageChops
 
-img = Image.open('img1.jpg')
+img = Image.open('in/img1.jpg')
 
 def tint_image(image, tint_color):
 	return ImageChops.multiply(image, Image.new('RGB', image.size, tint_color))
@@ -10,7 +10,7 @@ def tint_image(image, tint_color):
 for r in range(127, 255,32):
 	for g in range(127,255,32):
 		for b in range(127,255,32):
-			tint_image(img, (r,g,b)).save("trippy/" + str(r)+"_"+str(g)+"_"+str(b) + ".jpg")
+			tint_image(img, (r,g,b)).save("out/" + str(r)+"_"+str(g)+"_"+str(b) + ".jpg")
 			# tint_image(img, (r,g,b)).show()
 
 
